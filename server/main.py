58f566e3110889fce72069912c7d5947c19b95b3
Ramadhan <ramiqcom@gmail.com>
2025-02-01 20:21:07 +0100

    calculate land cover and rasterize category raster

--- server/main.py
+++ server/main.py
@@ -6,6 +6,7 @@ import rasterio as rio
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 from rasterio import windows
+from rasterio.features import rasterize
 
 app = FastAPI()
 
@@ -47,12 +48,46 @@ def biodiversity_post(item: Item):
         bounds = geom.total_bounds
         window = windows.from_bounds(*bounds, transform=transform)
         image = raster.read(
+            1,
             window=window,
             out_dtype="float32",
             boundless=True,
             fill_value=raster.nodata,
         )
         image[image == raster.nodata] = np.nan
+
+        # Create raster from geometry
+        raster_geom = rasterize(
+            [(geo, 1) for geo in geom.geometry],
+            out_shape=image.shape,
+            nodata=0,
+            fill=1,
+            dtype="uint8",
+            transform=windows.transform(window, transform),
+        )
+        mask = raster_geom == 0
+
+        # Mask image with raster geom
+        image[mask] = np.nan
+
+        # Land cover
+        lc = rio.open(
+            "https://storage.googleapis.com/gee-ramiqcom-s4g-bucket/hack4good_biodiversity/lc_category_nl_raster.tif"
+        )
+        lc_raster = lc.read(
+            1,
+            window=window,
+            out_dtype="float32",
+            boundless=True,
+            fill_value=lc.nodata,
+        )
+        lc_raster[(lc_raster == lc.nodata) | mask] = np.nan
+        lc_list = np.unique(lc_raster, return_counts=True)
+        lc_class = lc_list[0]
+        lc_areas = (lc_list[1] / 10) ** 2
+        lc_areas_dict = {}
+        for x in range(len(lc_class)):
+            lc_areas_dict[int(lc_class[x])] = round(float(lc_areas[x]), 2)
 
         # Calculate the score
         average_score = float(np.nanmean(image))
@@ -61,6 +96,9 @@ def biodiversity_post(item: Item):
             "score": round(average_score, 2),
             "credits": round(average_score * area / 1e6, 2),
             "geometry": geom_json,
+            "area": area,
+            "land_cover": lc_areas_dict,
         }
     except Exception as e:
+        print(e)
         raise HTTPException(status_code=404, detail=e)
